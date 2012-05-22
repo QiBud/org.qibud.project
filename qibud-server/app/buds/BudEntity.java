4cdb58c634d4666c3cddf7cd5a2dc95e43b3d2ea
Paul Merlin <paul@nosphere.org>
2012-05-23 01:19:15 +0200

    First steps towards Bud roles

--- qibud-server/app/buds/BudEntity.java
+++ qibud-server/app/buds/BudEntity.java
@@ -4,21 +4,18 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.Id;
-
 import play.modules.mongodb.jackson.MongoDB;
 
+import net.vz.mongodb.jackson.Id;
 import net.vz.mongodb.jackson.JacksonDBCollection;
 
-/**
- * About paging with MongoDB
- * https://groups.google.com/forum/?fromgroups#!topic/mongo-jackson-mapper/aEUuUPOowEw
- */
 public class BudEntity
         implements Serializable
 {
 
     public static final long _serialVersionUID = 1L;
+
+    private static final JacksonDBCollection<BudEntity, String> db = MongoDB.getCollection( "bud-entities", BudEntity.class, String.class );
 
     @Id
     public String identity;
@@ -31,28 +28,30 @@ public class BudEntity
 
     public static BudEntity findById( String id )
     {
-        return db().findOneById( id );
+        return db.findOneById( id );
     }
 
     public static void delete( BudEntity budEntity )
     {
-        db().removeById( budEntity.identity );
+        db.removeById( budEntity.identity );
     }
 
     public static void save( BudEntity budEntity )
     {
-        db().save( budEntity );
-        db().find().toArray();
+        db.save( budEntity );
     }
 
     public static List<BudEntity> findAll()
     {
-        return db().find().toArray();
+        return db.find().toArray();
     }
 
-    private static JacksonDBCollection<BudEntity, String> db()
+    public static List<BudEntity> findPage( int page )
     {
-        return MongoDB.getCollection( "buds", BudEntity.class, String.class );
+        if ( page < 1 ) {
+            page = 1;
+        }
+        return db.find().skip( ( page - 1 ) * 32 ).limit( 32 ).toArray();
     }
 
 }
--- qibud-server/app/samples/basebuds/PersonEntity.java
+++ qibud-server/app/samples/basebuds/PersonEntity.java
@@ -1,8 +1,19 @@
 package samples.basebuds;
 
+import play.modules.mongodb.jackson.MongoDB;
+
+import net.vz.mongodb.jackson.Id;
+import net.vz.mongodb.jackson.JacksonDBCollection;
+
 import roles.RoleEntity;
 
 public class PersonEntity
         implements RoleEntity
 {
+
+    private static final JacksonDBCollection<PersonEntity, String> db = MongoDB.getCollection( "person-entities", PersonEntity.class, String.class );
+
+    @Id
+    public String identity;
+
 }

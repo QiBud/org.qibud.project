8bc098ade5bc2e41316e1870f2c8ef2ef655e73d
Paul Merlin <paul@nosphere.org>
2012-09-18 16:47:06 +0200

    Started state role webservice

--- qibud-server/app/controllers/BudRoles.java
+++ qibud-server/app/controllers/BudRoles.java
@@ -22,16 +22,23 @@ import domain.roles.Role;
 import infrastructure.attachmentsdb.AttachmentsDB;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.math.BigDecimal;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.JsonNodeFactory;
 import org.codehaus.jackson.node.ObjectNode;
 import org.json.JSONException;
+import org.qi4j.api.association.AssociationStateDescriptor;
+import org.qi4j.api.common.QualifiedName;
 import org.qi4j.api.injection.scope.Service;
 import org.qi4j.api.injection.scope.Structure;
+import org.qi4j.api.json.JSONWriterSerializer;
+import org.qi4j.api.property.PropertyDescriptor;
 import org.qi4j.api.structure.Module;
 import org.qi4j.api.unitofwork.UnitOfWork;
 import org.qi4j.api.unitofwork.UnitOfWorkCompletionException;
+import org.qi4j.spi.Qi4jSPI;
+import org.qi4j.spi.entity.EntityState;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.With;
@@ -40,6 +47,9 @@ import play.mvc.With;
 public class BudRoles
         extends Controller
 {
+
+    @Structure
+    public static Qi4jSPI qi4j;
 
     @Structure
     public static Module module;
@@ -68,10 +78,31 @@ public class BudRoles
 
             ObjectMapper mapper = new ObjectMapper();
             ObjectNode json = JsonNodeFactory.instance.objectNode();
-            json.put( "identity", bud.identity().get() );
+            json.put( "bud-identity", bud.identity().get() );
+
             StringWriter descriptorWriter = new StringWriter();
             mapper.writeValue( descriptorWriter, roleDescriptor );
             json.put( "descriptor", mapper.readValue( descriptorWriter.toString(), JsonNode.class ) );
+
+            ObjectNode jsonState = JsonNodeFactory.instance.objectNode();
+            AssociationStateDescriptor entityStateDescriptor = qi4j.getEntityDescriptor( roleEntity ).state();
+            EntityState entityState = qi4j.getEntityState( roleEntity );
+            for ( PropertyDescriptor propertyDescriptor : entityStateDescriptor.properties() ) {
+                QualifiedName qualifiedName = propertyDescriptor.qualifiedName();
+                if ( "identity".equals( qualifiedName.name() ) ) {
+                    continue; // Skip Role identity
+                }
+                Object value = entityState.getProperty( qualifiedName );
+                String serialized = value == null ? "" : value.toString();
+                jsonState.put( qualifiedName.name(), serialized );
+            }
+
+            jsonState.put( "associations", JsonNodeFactory.instance.arrayNode() );
+            
+            jsonState.put( "many-associations", JsonNodeFactory.instance.objectNode() );
+
+            json.put( "state", jsonState );
+
             return ok( json );
         } finally {
             uow.discard();
